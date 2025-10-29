7801e36ccec388dd066ae81b705ce1682fa0a5ad
Vivekachooz <vivekachooz@gmail.com>
2025-10-29 11:39:54 +0530

    arm64: kernel: Enter charger mode when startupmode=dc_charger
    Change-Id: Ia255c507b1c1cfe1d01520755e7814a6c3e8ce9f

--- arch/arm64/kernel/setup.c
+++ arch/arm64/kernel/setup.c
@@ -294,12 +294,13 @@ static int __init androidboot_startupmode(char *p)
 {
 	char *offset_addr;
 
-	if (strcmp(p, "usb_charger"))
+	if (strcmp(p, "dc_charger"))
 		return 1;
 
-	if ((offset_addr =
-		     strstr(boot_command_line, "androidboot.mode=normal")))
-		memset(offset_addr, ' ', sizeof("androidboot.mode=normal"));
+	if ((offset_addr = strstr(boot_command_line, "androidboot.mode=normal"))) {
+		memcpy(offset_addr, "androidboot.mode=charger", sizeof("androidboot.mode=charger") - 1);
+		offset_addr[sizeof("androidboot.mode=charger") - 1] = ' ';
+	}
 
 	return 0;
 }
